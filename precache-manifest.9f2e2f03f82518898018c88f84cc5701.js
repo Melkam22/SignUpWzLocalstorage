39c1ddea4fb5c9be97500d2e0ef019740b3f6f14
andreas <andreass.ziegler@gmail.com>
2019-10-28 11:25:18 +0100

    Updates

---
+++ precache-manifest.9f2e2f03f82518898018c88f84cc5701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3e2b44e6ddf2e64e139f3fb50c5db1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1931a01497015376111d",
+    "url": "./static/css/main.25c23976.chunk.css"
+  },
+  {
+    "revision": "0e5ecd64fbf32ae591a1",
+    "url": "./static/js/2.9147c72a.chunk.js"
+  },
+  {
+    "revision": "1931a01497015376111d",
+    "url": "./static/js/main.d97a7f6c.chunk.js"
+  },
+  {
+    "revision": "ad336f20df20bf238f55",
+    "url": "./static/js/runtime-main.b23344ee.js"
+  }
+]);
